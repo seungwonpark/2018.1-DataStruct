4b77cd64353709267802277930be3c151ec2a12f
Seungwon Park <psw14041@gmail.com>
2018-04-16 14:11:44 +0900

    Calculator initial commit

--- 03_StackCalculator/CalculatorTest.java
+++ 03_StackCalculator/CalculatorTest.java
@@ -1,30 +1,25 @@
 import java.io.*;
+import java.util.*;
 
-public class CalculatorTest
-{
-	public static void main(String args[])
-	{
+public class CalculatorTest{
+	public static void main(String args[]){
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-		while (true)
-		{
-			try
-			{
+		while (true){
+			try{
 				String input = br.readLine();
-				if (input.compareTo("quit") == 0)
+				if (input.compareTo("q") == 0)
 					break;
 
 				command(input);
 			}
-			catch (Exception e)
-			{
+			catch (Exception e){
 				System.out.println("입력이 잘못되었습니다. 오류 : " + e.toString());
 			}
 		}
 	}
 
-	private static void command(String input)
-	{
+	private static void command(String input){
 		// TODO : 아래 문장을 삭제하고 구현해라.
 		System.out.println("<< command 함수에서 " + input + " 명령을 처리할 예정입니다 >>");
 	}
