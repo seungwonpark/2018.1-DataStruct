b650bf6d778c4805e88c11631eba308b39787f8f
Seungwon Park <psw14041@gmail.com>
2018-05-06 13:25:15 +0900

    Implement RS

--- 04_Sorting/SortingTest.java
+++ 04_Sorting/SortingTest.java
@@ -227,7 +227,39 @@ public class SortingTest
 	////////////////////////////////////////////////////////////////////////////////////////////////////
 	private static int[] DoRadixSort(int[] value)
 	{
-		// TODO : Radix Sort 를 구현하라.
+		final long INT_MAX = 214738456L;
+		final int RADIX = 4;
+		int n = value.length;
+		Long[] temp = new Long[n];
+		long maxval = 0;
+		for(int i=0; i<n; i++){
+			temp[i] = (Long.valueOf(value[i])) + INT_MAX;
+			if(maxval < temp[i]){
+				maxval = temp[i];
+			}
+		}
+		@SuppressWarnings("unchecked")
+		List<Long>[] buckets = new ArrayList[RADIX];
+		for(int i=0; i<4; i++){
+			buckets[i] = new ArrayList<Long>();
+		}
+		long mod = 1;
+		while(mod < maxval){
+			for(int i=0; i<n; i++){
+				buckets[(int)((temp[i] / mod) % RADIX)].add(temp[i]);
+			}
+			int cnt = 0;
+			for(int i=0; i<RADIX; i++){
+				for(Long x : buckets[i]){
+					temp[cnt++] = x;
+				}
+				buckets[i].clear();
+			}
+			mod *= 4;
+		}
+		for(int i=0; i<n; i++){
+			value[i] = Math.toIntExact(temp[i] - INT_MAX);
+		}
 		return (value);
 	}
 }
