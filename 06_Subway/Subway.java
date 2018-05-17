9aa166be355cc43174e6e19c1d3c491bb803e06c
Seungwon Park <psw14041@gmail.com>
2018-05-17 23:19:34 +0900

    implement reading from file

--- 06_Subway/Subway.java
+++ 06_Subway/Subway.java
@@ -1,16 +1,27 @@
 import java.util.*;
 import java.io.*;
 
-class Station{
-	String stNo;
+/*
+	Each station is represented as:
+	stNo, stName, lineName
+	(String), (String), (String)
+	1222, 복정, K2
+*/
+
+class Station{ // called by stNo. 
+	String stName;
+	String lineName;
 	ArrayList<Edge> road;
-	Station(String stNo){
-		this.stNo = stNo;
+	Station(String stName, String lineName){
+		this.stName = stName;
+		this.lineName = lineName;
 		this.road = new ArrayList<Edge>();
 	}
-	Station(String stNo, ArrayList<Edge> road){
-		this.stNo = stNo;
-		this.road = new ArrayList<Edge>(road);
+	public boolean equalStName(Station o){
+		return this.stName == o.stName;
+	}
+	public boolean equalLine(Station o){
+		return this.lineName == o.lineName;
 	}
 }
 
@@ -43,17 +54,31 @@ public class Subway{
 	public static void main(String args[]) throws IOException{
 		BufferedReader data = new BufferedReader(new FileReader(args[0]));
 		BufferedReader stdin = new BufferedReader(new InputStreamReader(System.in));
-		HashMap<String, String> st_line = new HashMap<String, String>();
-
+		HashMap<String, Station> db = new HashMap<String, Station>();
 		try{
+			// read and save station info.
 			String dataline;
-			while((dataline = data.readLine()) != null){ // until EOF
+			while(!(dataline = data.readLine()).equals("")){ // until blank line
 				String[] line = dataline.split(" ");
 				String stNo = line[0]; // station number (1222)
 				String stName = line[1]; // station name (복정)
 				String lineName = line[2]; // station line name (K2)
-				st_line.put(stNo, lineName);
+				db.put(stNo, new Station(stName, lineName));
 			}			
+		} catch (IOException e){
+			throw new IOException("Exception occured while reading file");
+		}
+		try{
+			// read and establish connection between stations.
+			String dataline;
+			while((dataline = data.readLine()) != null){ // until EOF
+				String[] line = dataline.split(" ");
+				String from = line[0];
+				String to = line[1];
+				long dist = Long.parseLong(line[2]);
+				db.get(from).road.add(new Edge(db.get(to), dist));
+			}
+			// TODO: 역 이름이 같은 stNo끼리 5분으로 잇기
 		} catch (IOException e){
 			throw new IOException("Exception occured while reading file");
 		}
