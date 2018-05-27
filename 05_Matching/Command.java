ac55e898de2735cc089950fbdae1f56f57624d79
Seungwon Park <psw14041@gmail.com>
2018-05-28 01:41:20 +0900

    Implement SearchPattern

--- 05_Matching/Command.java
+++ 05_Matching/Command.java
@@ -1,12 +1,12 @@
 import java.io.*;
 import java.util.*;
 public interface Command{
+	static final int HASH_LEN = 6;
 	void execute(MyHashTable<LinkedList<MyPair>> db) throws Exception;
 }
 
 class ReadFile implements Command{
 	private String arg;
-	static final int HASH_LEN = 6;
 	public ReadFile(String arg){
 		this.arg = arg;
 	}
@@ -63,7 +63,39 @@ class SearchPattern implements Command{
 	}
 	@Override
 	public void execute(MyHashTable<LinkedList<MyPair>> db) throws Exception{
-		// asdfasdf
-		System.err.println("to be implemented");
+		int l = arg.length();
+		String first_query = arg.substring(0, HASH_LEN);
+		LinkedList<MyPair> candidates = db.get(first_query);
+		if(candidates == null){
+			System.out.println("(0, 0)");
+			return;
+		}
+		// to avoid java.util.ConcurrentModificationException
+		LinkedList<MyPair> elected = new LinkedList<MyPair>(candidates);
+		for(int i=l; i>0; i-=HASH_LEN){
+			String query = arg.substring(i-HASH_LEN, i);
+			for(MyPair x : candidates){
+				boolean exists = false;
+				LinkedList<MyPair> test = db.get(query);
+				if(test != null){
+					for(MyPair y : test){
+						if(x.first == y.first && x.second + i - HASH_LEN == y.second){
+							exists = true;
+							break;
+						}
+					}
+				}
+				if(!exists){
+					elected.remove(x);
+				}
+			}
+		}
+
+		StringBuilder temp = new StringBuilder();
+		for(MyPair x : elected){
+			temp.append(x.toString());
+			temp.append(' ');
+		}
+		System.out.println(temp.toString().trim());
 	}
 }
